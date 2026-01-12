fee2789a24bfb8a16a4c68354a7bbea9a42585b8
Sandra-168 <dodgergreen@gmail.com>
2026-01-12 14:31:36 +0800

    分支 - java CI 練習

---
+++ src/main/java/com/practice/App.java
@@ -0,0 +1,12 @@
+package com.practice;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java CI is working!");
+    }
+
+    // 提供一個簡單的方法讓測試案例呼叫
+    public String getGreeting() {
+        return "Hello World";
+    }
+}
